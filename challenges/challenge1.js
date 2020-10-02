3eae8644f2bf3c1ce58027fb626605a1e8759c6b
Mathieu <mathieu.boez@gmail.com>
2020-10-02 13:36:22 +0200

    proper main loading challenges

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -1,10 +1,12 @@
-const readline = require('readline')
-const challengeDefinition = `\nChallenge 1
-Your goal is to create a function that removes the first and last letters of a string
-Strings with two characters or less are considered invalid.
-You can choose to have your function return null or simply ignore.\n\n\n`
+const readline = require('readline');
+const challengeDefinition = `
+    Challenge 1
+    Your goal is to create a function that removes the first and last letters of a string
+    Strings with two characters or less are considered invalid.
+    You can choose to have your function return null or simply ignore.\n\n\n
+`
 
-const askInput = `Please enter any string with at least 3 characters: `
+const askInputString = `Please enter any string with at least 3 characters: `
 
 const stringPeeler = (input) => {
     if (!input || !input.length) return 'Invalid String'
@@ -12,25 +14,26 @@ const stringPeeler = (input) => {
     return input.slice(1, input.length - 1)
 }
 
-const execute = async () => {
+const execute = () => {
     console.log(challengeDefinition)
 
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     })
-    rl.question(askInput, (input) => {
+    rl.question(askInputString, (input) => {
         console.log(stringPeeler(input))
         rl.close();
     })
 }
 
-exports.name = "Challenge 1"
-exports.definition = challengeDefinition
-exports.askInput = askInput
-exports.function = stringPeeler
+module.exports = {
+    name: 'Challenge 1',
+    definition: challengeDefinition,
+    askInputString: askInputString,
+    execute: execute(),
+}
 
-execute()
 /**
  * module.exports = {
     Cat: Cat
--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+const readline = require('readline')
+const fs = require('fs')
 
-const main = () => {
+const main = async () => {
+    const challengeAmount = fs.readdirSync('./challenges').length
 
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    rl.question(`
+        Which challenge do you want to run ?
+        Please enter a number between 1 and ${challengeAmount}
+    `, async (input) => {
+        const challenge = await require(`./challenges/challenge${input}`)
+        await challenge.execute
+    })
 }
 
 main()
