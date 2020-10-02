f117b52a5fb2b7548af1507a483e294d613d41db
Mathieu <mathieu.boez@gmail.com>
2020-10-02 14:27:07 +0200

    Challenge 2 done

--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -3,12 +3,48 @@ const challengeName = 'Daily Challenge #2 - String Diamond'
 
 const challengeDefinition = `
     ${challengeName}
+    Your task is to return a string that displays a diamond shape on the screen using asterisk (“*”) characters.
+    The shape that the print method will return should resemble a diamond. 
+    A number provided as input will represent the number of asterisks printed on the middle line. 
+    The line above and below will be centered and will have two less asterisks than the middle line. 
+    This reduction will continue for each line until a line with a single asterisk is printed at the top and bottom of the figure.
+
+    Return null if input is an even number or a negative number.
+
+    Note: JS and Python students must implement diamond() method and return None (Py) or null(JS) for invalid input.
 \n\n\n`
 
-const askInputString = `Please enter `
+const askInputString = `Please enter a positive, uneven number:`
 
 const challengeFunction = (input) => {
-    return null
+    if (input < 0 || input % 2 === 0) return null
+    let middleLine = (input + 1) / 2
+
+    let diamond = ''
+    for (let i = 1; i <= input; i++) {
+        // Get index of the middle line
+        const linesToMiddle = Math.abs(middleLine - i)
+
+        // Get number of spacing before first character
+        const spacing = ' '.repeat(linesToMiddle * 2)
+
+        // Get amount of characters on the line
+        const nbCharacters = input - (2 * linesToMiddle)
+
+        // Get characters line
+        let characters = ''
+        for (let j = 1; j <= nbCharacters; j++) {
+            if (j == nbCharacters) {
+                characters += '*'  // No spacing after last one to allow spacing variable usage twice
+            } else {
+                characters += '* '
+            }
+        }
+        const line = spacing + characters + spacing + '\n'
+        diamond += line
+    }
+    // Write the diamond
+    console.log(diamond)
 }
 
 const execute = () => {
@@ -19,7 +55,7 @@ const execute = () => {
         output: process.stdout
     })
     rl.question(askInputString, (input) => {
-        console.log(challengeFunction(input))
+        console.log(challengeFunction(parseInt(input)))
         rl.close();
     })
 }
--- main.js
+++ main.js
@@ -4,14 +4,15 @@ const fs = require('fs')
 const main = async () => {
     const challengeAmount = fs.readdirSync('./challenges').length -1 // -1 because of template
 
-    const rl = readline.createInterface({
+    const lineReader = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     })
-    rl.question(`
+    lineReader.question(`
         Which challenge do you want to run ?
         Please enter a number between 1 and ${challengeAmount}
     `, async (input) => {
+        lineReader.close()
         const challenge = await require(`./challenges/challenge${input}`)
         await challenge.execute
     })
